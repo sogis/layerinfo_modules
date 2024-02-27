63962cddfea9a44a31a5ddb09c890d783693d3f5
Oliver Jeker <24891004+ojeker@users.noreply.github.com>
2024-02-27 15:12:21 +0100

    Delete heatdrill directory

--- heatdrill/pyruntime/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python3.8 -m venv venv38
-venv38/bin/pip3 install -r requirements.txt
--- heatdrill/runtests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pyruntime/venv38/bin/python3.8 -m unittest -v src/heatdrill_test.py
